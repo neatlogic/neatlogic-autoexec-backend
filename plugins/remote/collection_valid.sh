0bdf48918ced9f0694250cc7c6b1d04e8bb252cd
温海波 <wenhb@techsure.com.cn>
2021-06-28 18:22:01 +0800

    cmdb采集插件目录调整

--- plugins/remote/collection_valid.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for file in `ls |grep collection_ |grep -v collection_valid`;do
-   perl -c $file/$file;
-   perltidy -i=4 -ci=4 -l=200 -b $file/$file;
-   rm -f "${file}/$file.bak";
-done
