43339fd5c1f3c24aa411f198d6866ed9d211789d
温海波 <wenhb@techsure.com.cn>
2021-07-09 19:15:14 +0800

    BASE update

--- plugins/remote/cmdbcollect/lib/BASECollector.pm
+++ plugins/remote/cmdbcollect/lib/BASECollector.pm
@@ -42,16 +42,18 @@ sub getConfig {
     return {};
 }
 
-#采集数据兑现的Primary Key设置，只需要在返回多种类型对象的收集器里定义
+#采集数据对象的Primary Key设置，只需要在返回多种类型对象的收集器里定义
 #注意：！！如果是返回单类型对象的采集器不需要定义此函数，可以删除此函数
 sub getPK {
     my ($self) = @_;
     return {
         #默认KEY用类名去掉Collector，对应APP_TYPE属性值
         #配置值就是作为PK的属性名
-        $self->{defaultAppType} => [ 'MANAGE_IP', 'PORT', ]
-
-            #如果返回的是多种对象，需要手写APP_TYPE对应的PK配置
+        $self->{defaultAppType} => [
+            'MANAGE_IP',
+            'PORT',
+        ]
+        #如果返回的是多种对象，需要手写APP_TYPE对应的PK配置
     };
 }
 
