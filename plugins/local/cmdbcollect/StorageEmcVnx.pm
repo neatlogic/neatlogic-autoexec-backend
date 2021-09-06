e0ec99d5c6c7606cc8829d4201388f7b97d4d76b
温海波 <wenhb@techsure.com.cn>
2021-09-06 16:00:51 +0800

    storage cmdb

--- plugins/local/cmdbcollect/StorageEmcVnx.pm
+++ plugins/local/cmdbcollect/StorageEmcVnx.pm
@@ -16,20 +16,19 @@ sub new {
     if ( not defined($binPath) or $binPath == '' ) {
         $binPath = abs_path("$FindBin::Bin/../../../tools/Navisphere/bin/naviseccli");
     }
-    my $host     = $args{host};
-    my $user     = $args{user};
-    my $password = $args{password};
+    my $node     = $args{node};
+    $self->{node} = $node;
 
-    $self->{host}     = $host;
-    $self->{user}     = $user;
-    $self->{password} = $password;
-    $self->{binPath}  = $binPath;
+    my $host     = $node->{host};
+    my $user     = $node->{user};
+    my $password = $node->{password};
 
-    my $naviseccli = "$binPath -h $host -Scope 0 -User $user -Password $password";
-    $self->{naviseccli} = $naviseccli;
+    my $naviSecCLI = "$binPath -h $host -Scope 0 -User $user -Password $password";
+    $self->{naviSecCLI} = $naviSecCLI;
 
     my $utils = CollectUtils->new();
     $self->{collectUtils} = $utils;
+    
     bless( $self, $type );
     return $self;
 }
@@ -37,18 +36,18 @@ sub new {
 sub getCmdOut {
     my ( $self, %args ) = @_;
     my $utils      = $self->{collectUtils};
-    my $naviseccli = $self->{naviseccli};
+    my $naviSecCLI = $self->{naviSecCLI};
     my $command    = $args{cmd};
-    my $cmd        = "$naviseccli $command";
+    my $cmd        = "$naviSecCLI $command";
     return $utils->getCmdOut( $cmd );
 }
 
 sub getCmdOutLines {
     my ( $self, %args ) = @_;
     my $utils      = $self->{collectUtils};
-    my $naviseccli = $self->{naviseccli};
+    my $naviSecCLI = $self->{naviSecCLI};
     my $command    = $args{cmd};
-    my $cmd        = "$naviseccli $command";
+    my $cmd        = "$naviSecCLI $command";
     return $utils->getCmdOutLines( $cmd );
 }
 
@@ -56,77 +55,81 @@ sub collect {
     my ($self) = @_;
     my $data   = {};
     my $out    = $self->getCmdOut('getagent');
+
+    $data->{VENDOR} = 'Dell';
+    $data->{BRAND}  = 'EMC';
+    $data->{UPTIME} = undef;
+
     if ( $out =~ /Model:\s+(\S+)\s+/ ) {
         $data->{MODEL} = $1;
     }
     if ( $out =~ /Serial\s+No:\s+(\S+)\s+/ ) {
-        $data->{SERIAL_NO} = $1;
+        $data->{SN} = $1;
     }
     if ( $out =~ /Name:\s+(\S+)\s+/ ) {
-        $data->{HOSTNAME} = $1;
+        $data->{DEV_NAME} = $1;
     }
 
-    my $lun_out = $self->getCmdOut('getlun');
-    my @arr_lun = $lun_out =~ /LOGICAL UNIT NUMBER(.*?)MirrorView/sg;
+    my $lunOutInfo = $self->getCmdOut('getlun');
+    my @lunInfoLines = $lunOutInfo =~ /LOGICAL UNIT NUMBER(.*?)MirrorView/sg;
+
+    my $lunsMap = {};
     my @luns    = ();
-    foreach my $line (@arr_lun) {
+    foreach my $line (@lunInfoLines) {
         $line =~ s/^\s+|\s+$//g;
 
         my ( $name, $uuid, $size, $id );
         if ( $line =~ /^(\d+)\s+/ ) {
             $id = $1;
         }
-        if ( $line =~ /Name\s*(.+)\n/ ) {
+        elsif ( $line =~ /Name\s*(.+?)\s*\n/ ) {
             $name = $1;
-            $name =~ s/^\s+|\s+$//g;
         }
-        if ( $line =~ /UID:\s*(\S+)\s*/ ) {
+        elsif ( $line =~ /UID:\s*(\S+)\s*/ ) {
             $uuid = $1;
         }
-        if ( $line =~ /LUN\s*Capacity\(Megabytes\):\s*(\d+)\s*/ ) {
+        elsif ( $line =~ /LUN\s*Capacity\(Megabytes\):\s*(\d+)\s*/ ) {
             $size = $1;
-            $size = int( $size / 1024 );
+            $size = int( $size * 100 / 1024 + 0.5 )/100;
         }
 
         my $lun = {};
         $lun->{ID}   = $id;
         $lun->{NAME} = $name;
-        $lun->{UUID} = $uuid;
+        $lun->{LUNID} = $uuid;
         $lun->{SIZE} = $size;
         push( @luns, $lun );
+        $lunsMap->{$id} = $lun;
     }
     $data->{LUNS} = \@luns;
 
-    my $pool_out = $self->getCmdOut('getrg');
-    my @arr_pool = $pool_out =~ /RaidGroup ID:(.*?)Legal RAID types:/sg;
+    my $poolOutInfo = $self->getCmdOut('getrg');
+    my @poolInfoLines = $poolOutInfo =~ /RaidGroup ID:(.*?)Legal RAID types:/sg;
+
     my @pools    = ();
-    foreach my $line (@arr_pool) {
+    foreach my $line (@poolInfoLines) {
         $line =~ s/^\s+|\s+$//g;
 
         my ( $name, $size, $free );
         if ( $line =~ /^(\d+)\s+/ ) {
             $name = $1;
         }
-        if ( $line =~ /Logical Capacity \(Blocks\):\s+(\S+)\s+/ ) {
+        elsif ( $line =~ /Logical Capacity \(Blocks\):\s+(\S+)\s+/ ) {
             $size = $1;
-            $size = int( $size / 2048 / 1024 );
-            $size = $size . 'GB';
+            $size = int( $size * 100 / 2048 / 1024 + 0.5 ) / 100;
+            $size = $size;
         }
-        if ( $line =~ /Free Capacity \(Blocks,non-contiguous\):\s+(\d+)\s+/ ) {
+        elsif ( $line =~ /Free Capacity \(Blocks,non-contiguous\):\s+(\d+)\s+/ ) {
             $free = $1;
-            $free = int( $free / 2048 / 1024 );
-            $free = $free . 'GB';
+            $free = int( $free * 100 / 2048 / 1024 + 0.5 ) / 100;
+            $free = $free;
         }
 
-        my @pool_lun = ();
-        if ( $line =~ /List of luns:\s*(.*)\s*/ ) {
-            my $pool_lunid = $1;
-            $pool_lunid =~ s/^\s+|\s+$//g;
-            foreach my $pool (@luns) {
-                my $rid = $pool->{ID};
-                if ( $rid eq $pool_lunid ) {
-                    push( @pool_lun, { 'VALUE' => $rid } );
-                }
+        my @lunsInPool = ();
+        if ( $line =~ /List of luns:\s*(.*?)\s*/ ) {
+            my $lunId = $1;
+            if (defined($lunsMap->{$lunId})){
+                push(@lunsInPool, $lunsMap->{$lunId});
             }
         }
 
@@ -134,7 +137,7 @@ sub collect {
         $pool->{NAME}        = $name;
         $pool->{SIZE}        = $size;
         $pool->{FREE}        = $size;
-        $pool->{CONTAIN_LUN} = \@pool_lun;
+        $pool->{LUNS} = \@pool_lun;
 
         push( @pools, $pool );
     }
