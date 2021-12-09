876884fc8f6a7c68e93e3414a7aad5cf68fb12be
温海波 <wenhb@techsure.com.cn>
2021-12-09 11:23:47 +0800

    增加老化作业和日志的清理的功能

---
+++ lib/JobPurger.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+ Copyright © 2017 TechSure<http://www.techsure.com.cn/>
+"""
+import os
+import time
+import shutil
+
+
+class JobPurger:
+    def __init__(self, context, reserveDays):
+        self.context = context
+        self.reserveDays = reserveDays
+        self.reserveSeconds = reserveDays * 86400
+        self.nowTime = time.time()
+        self.jobDirIdx = 0
+
+    def purgeJob(self, absRoot):
+        if os.path.exists(absRoot):
+            for item in os.scandir(absRoot):
+                if item.is_dir():
+                    self.purgeJob(item)
+                else:
+                    if item.name == 'params.json':
+                        paramFile = item.path
+                        jobIdPath = paramFile[0:-12]
+                        jobMtime = os.stat(paramFile).st_mtime
+                        if self.nowTime - jobMtime > self.reserveSeconds:
+                            shutil.rmtree(jobIdPath)
+                            print("INFO: Remove job dictory:", jobIdPath)
+
+    def delExpiredLog(self, hislogRoot):
+        for item in os.scandir(hislogRoot):
+            if item.is_file():
+                fileMtime = os.stat(item.path).st_mtime
+                if self.nowTime - fileMtime > self.reserveSeconds:
+                    os.unlink(item.path)
+                    print("INFO: Remove expired history log:", item.path)
+
+    def purgeHisLog(self, absRoot):
+        for item in os.scandir(absRoot):
+            if item.is_dir():
+                if item.name.endswith('.hislog'):
+                    self.delExpiredLog(item.path)
+                else:
+                    self.purgeHisLog(item)
+
+    def purge(self):
+        jobPath = self.context.dataPath + '/job'
+        self.jobDirIdx = len(jobPath) + 1
+        self.purgeJob(jobPath)
+        self.purgeHisLog(jobPath)
