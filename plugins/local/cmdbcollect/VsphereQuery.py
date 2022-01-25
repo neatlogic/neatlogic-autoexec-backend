afc3e8154105ae7bb5f2ece047349fae3e60161d
罗宇 <luoyu@techsure.com.cn>
2022-01-25 16:15:01 +0800

    adjust vcenter

--- plugins/local/cmdbcollect/VsphereQuery.py
+++ plugins/local/cmdbcollect/VsphereQuery.py
@@ -191,19 +191,20 @@ class VsphereQuery:
         serialNumber = host.hardware.systemInfo.serialNumber
         ins['MACHINE_UUID'] = host.hardware.systemInfo.uuid
         ins['MACHINE_SN'] = serialNumber
-        ins['HOST_ON'] = [{'_OBJ_CATEGORY': 'HOST', '_OBJ_TYPE': 'HOST', 'BOARD_SERIAL': serialNumber}]
-        ins['CLUSTERED_ON'] = [{'_OBJ_CATEGORY': 'VIRTUALIZED', '_OBJ_TYPE': 'VCENTER', 'MOID': cluster._moId}]
+        ins['HOST_ON'] = [{'_OBJ_CATEGORY': 'HOST', '_OBJ_TYPE': 'HOST', 'BOARD_SERIAL': serialNumber,'_OBJ_CATEGORY': 'HOST', '_OBJ_TYPE': 'HOST'}]
+        ins['CLUSTERED_ON'] = [{'_OBJ_CATEGORY': 'VIRTUALIZED', '_OBJ_TYPE': 'VCENTER', 'MOID': cluster._moId,'_OBJ_CATEGORY': 'VMWARE-CLUSTER', '_OBJ_TYPE': 'VMWARE-CLUSTER','MGIT_IP':self.ip}]
         return ins
 
     def get_vmlist(self, cluster):
         data_list = []
-        host_list = cluster.host
-        if host_list != None:
-            for host in host_list:
-                vm_list = host.vm
-                if vm_list != None:
-                    for vm in vm_list:
-                        data_list.append(self.get_vm(host, vm, cluster))
+        if(hasattr(cluster ,'host')):
+            host_list = cluster.host
+            if host_list != None:
+                for host in host_list:
+                    vm_list = host.vm
+                    if vm_list != None:
+                        for vm in vm_list:
+                            data_list.append(self.get_vm(host, vm, cluster))
         return data_list
 
     def collect(self):
