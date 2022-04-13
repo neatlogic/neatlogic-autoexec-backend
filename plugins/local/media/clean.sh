7e73ad250a52cee33175760d77e46c1dbbe5c1dd
温海波 <wenhb@techsure.com.cn>
2022-04-13 13:48:42 +0800

    perl media

--- plugins/local/media/clean.sh
+++ plugins/local/media/clean.sh
@@ -11,32 +11,13 @@ fi
 
 install_base=$PL_LIB_PATH
 
-if [ ! "$#" = "1" ]; then
-  if [ -z "$TECHSURE_HOME" ]; then
-    echo "\$TECHSURE_HOME not defined, rerun it after export TECHSURE_HOME=/xxxx"
-    exit -1
-  fi
-else
-  install_base=$1
-fi
-
 cd $PERL_MEDIA_HOME/perl-pkgs || exit 1
-
-echo "Delete share lib(so file) in $install_base"
-#find . -name '*.so' -not -name 'Oracle.so' -not -name 'mysql.so'
-find $install_base -name '*.so' -not -name 'mysql.so' -exec rm -f {} \;
 
 echo "Delete directory in media path"
 for dir in $(find . -maxdepth 1 -type d); do
   if [ "$dir" != "." -a "$dir" != ".." ]; then
     rm -rf $dir
   fi
-done
-
-echo "extrace perl packages in media path"
-for file in $(find . -maxdepth 1 -type f); do
-  echo "untar $file"
-  tar -xmzvf $file
 done
 
 cd $PRE_CWD
--- plugins/local/media/setup.sh
+++ plugins/local/media/setup.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 PRE_CWD=$(pwd)
 cd $(dirname $0)
 PERL_MEDIA_HOME=$(pwd)
 PL_LIB_PATH=$(cd "$PERL_MEDIA_HOME/../pllib" && pwd)
+AUTOEXEC_HOME=$(cd "$PERL_MEDIA_HOME/../../.." && pwd)
 
 if [ "$PL_LIB_PATH" == "" ]; then
         echo "Can not determin the perl lib path, exit."
@@ -10,6 +16,7 @@ if [ "$PL_LIB_PATH" == "" ]; then
 fi
 
 echo "MEDIA HOME:$PERL_MEDIA_HOME"
+echo "AUTOEXEC HOME:$AUTOEXEC_HOME"
 
 export PERL5LIB=.:$PL_LIB_PATH/lib/perl5:$PERL5LIB
 
@@ -17,57 +24,80 @@ install_base=$PL_LIB_PATH
 
 cd $PERL_MEDIA_HOME/perl-pkgs || exit 1
 
-#echo "Delete share lib(so file) in $install_base"
-#find $install_base -name '*.so' -exec rm -f {} \;
+echo "Delete share lib(so file) in $install_base..."
+#find . -name '*.so' -not -name 'Oracle.so' -not -name 'mysql.so' -not -name 'db2.so'
+find $install_base -name '*.so' -not -name 'Oracle.so' -not -name 'mysql.so' -not -name 'db2.so' -exec rm -f {} \;
 
-#echo "Delete directory in media path"
-#for dir in `find . -maxdepth 1 -type d`
-#do
-#  if [ "$dir" != "." -a "$dir" != ".." ]
-#  then
-#    rm -rf $dir
-#  fi
-#done
+echo "Delete directory in media path"
+for dir in $(find . -maxdepth 1 -type d); do
+        if [ "$dir" != "." -a "$dir" != ".." ]; then
+                echo -e "${BLUE}Delete directory $dir${NC}"
+                rm -rf $dir
+                if [[ $? != 0 ]]; then
+                        echo -e "${RED}ERROR: Remove $dir failed.${NC}"
+                fi
+        fi
+done
 
-#echo "extrace perl packages in media path"
-#for file in `find . -maxdepth 1 -type f`
-#do
-#	echo "untar $file"
-#        tar -xmzvf $file
-#done
+echo "Extrace perl packages in $PERL_MEDIA_HOME..."
+for file in $(find . -maxdepth 1 -type f); do
+        echo -e "${BLUE}Extract file $file${NC}"
+        tar -xzf $file
+        if [[ $? != 0 ]]; then
+                echo -e "${RED}ERROR: Extract $file failed.${NC}"
+        fi
+done
 
+declare -a INS_SUC_PKGS
+declare -a INS_FAIL_PKGS
+declare -i INSTALL_COUNT=0
 echo "Begin install perl pkgs......"
 pwd
 
 for dir in $(find . -maxdepth 1 -type d); do
-        if [ -e "$dir/Build.PL" ]; then
+        dir=${dir#*/}
+        if [[ $dir == DBD-Oracle* ]]; then
+                #oracle DBD
+                echo -e "${BLUE}Try to install package:${dir}${NC}"
                 cd $dir
-                perl Build.PL --install_base $install_base
-                #perl Build.PL --prefix $install_base
-                ./Build install
-                cd ..
-        fi
-
-        if [ -e "$dir/Makefile.PL" ]; then
-                cd $dir
-                perl Makefile.PL INSTALL_BASE=$install_base
-                #perl Makefile.PL PREFIX=$install_base
-                make
-                make install
-                cd ..
-        fi
-done
-
-#oracle DBD
-for dir in DBD-Oracle*; do
-        if [ -d $dir ]; then
-                cd $dir
-                export ORACLE_HOME=$TECHSURE_HOME/ezdeploy/tools/oracle-client
+                export ORACLE_HOME=$AUTOEXEC_HOME/tools/oracle-client
                 export LD_LIBRARY_PATH=$ORACLE_HOME/lib
-                perl Makefile.PL INSTALL_BASE=$TECHSURE_HOME/ezdeploy/lib/perl-lib -V 12.1.0 -h $ORACLE_HOME/sdk/include
-                make
-                make install
-                cd ..
+                perl Makefile.PL INSTALL_BASE=$install_base -V 12.1.0 -h $ORACLE_HOME/sdk/include && make && make install
+                if [[ $? -ne 0 ]]; then
+                        echo -e "${RED}ERROR: Install package:$dir failed.${NC}"
+                        INS_FAIL_PKGS+=($dir)
+                else
+                        echo -e "${GREEN}Package:$dir install success.${NC}"
+                        INS_SUC_PKGS+=($dir)
+                fi
+                cd $PERL_MEDIA_HOME/perl-pkgs
+                INSTALL_COUNT=INSTALL_COUNT+1
+        elif [[ -e "$dir/Build.PL" ]]; then
+                echo -e "${BLUE}Try to install package:${dir}${NC}"
+                cd $dir
+                perl Build.PL --install_base $install_base && ./Build install
+                if [[ $? -ne 0 ]]; then
+                        echo -e "${RED}ERROR: Install package:$dir failed.${NC}"
+                        INS_FAIL_PKGS+=($dir)
+                else
+                        echo -e "${GREEN}Package:$dir install success.${NC}"
+                        INS_SUC_PKGS+=($dir)
+                fi
+                cd $PERL_MEDIA_HOME/perl-pkgs
+                INSTALL_COUNT=INSTALL_COUNT+1
+        elif [[ -e "$dir/Makefile.PL" ]]; then
+                echo -e "${BLUE}Try to install package:${dir}${NC}"
+                cd $dir
+                perl Makefile.PL INSTALL_BASE=$install_base && make && make install
+                if [[ $? -ne 0 ]]; then
+                        echo -e "${RED}ERROR: Install package:$dir failed.${NC}"
+                        INS_FAIL_PKGS+=($dir)
+                else
+                        echo -e "${GREEN}Package:$dir install success.${NC}"
+                        INS_SUC_PKGS+=($dir)
+                fi
+                cd $PERL_MEDIA_HOME/perl-pkgs
+                INSTALL_COUNT=INSTALL_COUNT+1
         fi
 done
 
@@ -75,5 +105,18 @@ done
 unalias cp >/dev/null 2>&1
 echo "cp -rf $PERL_MEDIA_HOME/perl-lib/* $install_base/"
 cp -rf $PERL_MEDIA_HOME/perl-lib/* $install_base/
+
+if [[ $INSTALL_COUNT == 0 ]]; then
+        echo -e "${RED}ERROR: Can not find any pakcages untar directory.${NC}"
+        echo -e "${RED}Please execute clean.sh before.${NC}"
+fi
+
+for PKG in ${INS_SUC_PKGS[@]}; do
+        echo -e "${GREEN}${PKG} install success.${NC}"
+done
+
+for PKG in ${INS_FAIL_PKGS[@]}; do
+        echo -e "${RED}${PKG} install failed.${NC}"
+done
 
 cd $PRE_CWD
