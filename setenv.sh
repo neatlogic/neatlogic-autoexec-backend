97cbc9c7b1251fd6e5c3cae3b9ae0d985649c5c1
温海波 <wenhb@techsure.com.cn>
2022-05-12 11:44:10 +0800

    修改说明

--- setenv.sh
+++ setenv.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/bash
-SCRIPT_DIR=$(dirname "$SOURCE_PATH")
-if [ "$SCRIPT_DIR"="." ]
-then
-    SCRIPT_DIR=$(pwd)
-fi
+PROG_PATH=${BASH_SOURCE[0]}
+echo $PROG_PATH
+SCRIPT_DIR=$(cd $(dirname "$PROG_PATH") && pwd)
 
-echo "NOTICE：Please source this script:. ./setenv.sh"
+echo "NOTICE：Please source this script:. $SCRIPT_DIR/setenv.sh"
 
 export PYTHONPATH=.:$SCRIPT_DIR/plugins/local/lib:$SCRIPT_DIR/lib:$SCRIPT_DIR/plib
-#export PERL5LIB=.:$SCRIPT_DIR/plugins/local/lib:$SCRIPT_DIR/plugins/local/lib/perl-lib/lib/perl5
+export PERL5LIB=.:$SCRIPT_DIR/plugins/local/lib:$SCRIPT_DIR/plugins/local/pllib/lib/perl5
 export PATH=.:$SCRIPT_DIR/plugins/local/bin:$SCRIPT_DIR/plugins/local/tools:$SCRIPT_DIR/plugins/local/bin:$PATH
 
 echo PYTHONPATH=$PYTHONPATH
 echo PERL5LIB=$PERL5LIB
 echo PATH=$PATH
-
