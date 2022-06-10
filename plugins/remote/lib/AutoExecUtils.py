5e848e78e000c625b927f03dccf8abb01a0cd699
温海波 <wenhb@techsure.com.cn>
2022-06-10 18:46:07 +0800

    python remote lib

---
+++ plugins/remote/lib/AutoExecUtils.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+ Copyright © 2017 TechSure<http://www.techsure.com.cn/>
+"""
+
+import os
+import sys
+import json
+
+
+def setEnv():
+    modPaths = os.path.split(os.path.realpath(__file__))
+    binPath = os.path.realpath(modPaths[0]+'/..')
+    outputPath = os.path.join(binPath, 'output.json')
+    os.environ['OUTPUT_PATH'] = outputPath
+    hidePwdInCmdLine()
+
+
+def hidePwdInCmdLine():
+    pass
+
+
+def saveOutput(outputData):
+    modPaths = os.path.split(os.path.realpath(__file__))
+    binPath = os.path.realpath(modPaths[0]+'/..')
+    outputPath = os.path.join(binPath, 'output.json')
+    print("INFO: Try save output to {}.\n".format(outputPath))
+    if outputPath is not None and outputPath != '':
+        outputFile = open(outputPath, 'w')
+        outputFile.write(json.dumps(outputData, indent=4, ensure_ascii=False))
+        outputFile.close()
+    else:
+        print("WARN: Could not save output file, because of environ OUTPUT_PATH not defined.\n")
