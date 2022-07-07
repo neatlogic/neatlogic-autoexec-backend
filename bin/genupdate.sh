216f8f24655600642a081af435909d9978239889
温海波 <wenhb@techsure.com.cn>
2022-07-07 19:28:39 +0800

    genupdate

--- bin/genupdate.sh
+++ bin/genupdate.sh
@@ -9,4 +9,4 @@ fi
 
 #cd "$AUTOEXEC_DIR" && tar -cvzf /tmp/autoexec.update.tgz --exclude plugins/local/pllib bin discovery i18n lib media plugins
 
-cd "$AUTOEXEC_DIR" && tar -cvzf /tmp/autoexec.update.tgz --exclude plugins/local/pllib --exclude plugins/local/media bin i18n plugins
+cd "$AUTOEXEC_DIR" && tar -cvzf /tmp/autoexec.update.tgz --exclude plugins/local/pllib --exclude plugins/local/media bin lib i18n plugins
