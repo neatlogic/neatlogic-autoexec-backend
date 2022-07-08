bbc85caea53ba5938c5bf6c296224023706dd938
温海波 <wenhb@techsure.com.cn>
2022-07-08 16:16:08 +0800

    gen package script

--- bin/genupdate.sh
+++ bin/genupdate.sh
@@ -7,6 +7,6 @@ if [[ -e "/tmp/autoexec.tgz" ]]; then
     rm -f "/tmp/autoexec.tgz"
 fi
 
-#cd "$AUTOEXEC_DIR" && tar -cvzf /tmp/autoexec.update.tgz --exclude plugins/local/pllib bin discovery i18n lib media plugins
+#cd "$AUTOEXEC_DIR" && tar -cvzf /tmp/autoexec.update.tgz --exclude .DS_Store --exclude __pycache__ --exclude plugins/local/pllib bin discovery i18n lib media plugins
 
-cd "$AUTOEXEC_DIR" && tar -cvzf /tmp/autoexec.update.tgz --exclude plugins/local/pllib --exclude plugins/local/media bin lib i18n plugins
+cd "$AUTOEXEC_DIR" && tar -cvzf /tmp/autoexec.update.tgz --exclude .DS_Store --exclude __pycache__ --exclude plugins/local/pllib --exclude plugins/local/media bin lib i18n plugins
