3fc9a6bb8ac4cace33c82f7638323783fe866826
lvzk <lvzk@techsure.com.cn>
2022-07-21 11:56:53 +0800

    update 执行记录文件名格式

--- plugins/local/deploy/lib/SQLFileRunner.pm
+++ plugins/local/deploy/lib/SQLFileRunner.pm
@@ -188,7 +188,7 @@ sub execOneSqlFile {
         }
     }
     my $dateTimeStr    = strftime( "%Y%m%d-%H%M%S", localtime() );
-    my $hisLogName     = "$dateTimeStr-running-$ENV{AUTOEXEC_USER}.txt";
+    my $hisLogName     = "$dateTimeStr.running.$ENV{AUTOEXEC_USER}.txt";
     my $hisLogFilePath = "$hisLogDir/$hisLogName";
 
     if ( -e $logFilePath ) {
@@ -245,7 +245,7 @@ sub execOneSqlFile {
             if ( defined($sqlFileStatus) ) {
                 my $endStatus     = $sqlFileStatus->loadAndGetStatusValue('status');
                 my $newHisLogName = $hisLogName;
-                $newHisLogName =~ s/-running-/-$endStatus-/;
+                $newHisLogName =~ s/.running./.$endStatus./;
                 my $newHisLogPath = "$hisLogDir/$newHisLogName";
                 rename( $hisLogFilePath, $newHisLogPath );
             }
