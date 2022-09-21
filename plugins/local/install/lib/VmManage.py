a08f15cee45b9a2c9a882e2861ff521089dcd1e3
luoyu <luoyu@techsure.com.cn>
2022-09-21 15:49:56 +0800

    vm manager

--- plugins/local/install/lib/VmManage.py
+++ plugins/local/install/lib/VmManage.py
@@ -92,47 +92,35 @@ class VmManage(object):
         objView.Destroy()
         return vmList
 
-    def get_physical_cdrom(self ,host):
-        for lun in host.configManager.storageSystem.storageDeviceInfo.scsiLun:
-            if lun.lunType == 'cdrom':
-                return lun
-        return None
-
-    def find_free_ide_controller(self ,vm):
-        for dev in vm.config.hardware.device:
-            if isinstance(dev, vim.vm.device.VirtualIDEController):
-                if len(dev.device) < 2:
-                    return dev
-        return None
-
     def find_device(self , vm, device_type):
         result = []
         for dev in vm.config.hardware.device:
             if isinstance(dev, device_type):
                 result.append(dev)
         return result
+        
+    def _get_folder(self,name):
+        obj = None
+        container = self.content.viewManager.CreateContainerView(self.content.rootFolder, [vim.Folder], True)
+        for managed_object_ref in container.view:
+            if managed_object_ref.name == name:
+                obj = managed_object_ref
+                break
+        container.Destroy()
+        return obj
 
+    def create_folder(self,host_folder, folder_name):
+        tm_folder = self._get_folder(folder_name)
+        if tm_folder is None :
+            host_folder.CreateFolder(folder_name)
 
-    def new_cdrom_spec(self , controller_key, backing):
-        connectable = vim.vm.device.VirtualDevice.ConnectInfo()
-        connectable.allowGuestControl = True
-        connectable.startConnected = True
-
-        cdrom = vim.vm.device.VirtualCdrom()
-        cdrom.controllerKey = controller_key
-        cdrom.key = -1
-        cdrom.connectable = connectable
-        cdrom.backing = backing
-        return cdrom
-
-    def get_customspec(self, param):
+    def clone_customspec(self, param):
         nicSpec = vim.vm.customization.IPSettings()
         nicSpec.ip = vim.vm.customization.FixedIp()
         nicSpec.ip.ipAddress = str(param['vm_ip'])
         nicSpec.subnetMask = str(param['netmask'])
         nicSpec.gateway = str(param['gateway'])
         #nicSpec.dnsDomain = dnsDomain
-        print(nicSpec)
         prep = None
         if param['os_type'] == 'windows':
             prep = vim.vm.customization.Sysprep()
@@ -176,7 +164,7 @@ class VmManage(object):
         vm = self._get_obj([vim.VirtualMachine], param['vm_name'])
         if vm is None :
             return -1
-        print("INFO:: VM {} add disk start.".format(param['vm_name']))
+        
         for device in vm.config.hardware.device:
             if hasattr(device.backing, 'fileName'):
                 unit_number = int(device.unitNumber) + 1
@@ -191,6 +179,7 @@ class VmManage(object):
         if controller is None:
             print("WARN::Disk SCSI controller not found!")
             return -1
+        
         # add disk here
         dev_changes = []
         new_disk_kb = int(param['disk_size']) * 1024 * 1024
@@ -208,10 +197,13 @@ class VmManage(object):
         disk_spec.device.controllerKey = controller.key
         dev_changes.append(disk_spec)
         spec.deviceChange = dev_changes
-        self.wait_for_task('add disk',vm.ReconfigVM_Task(spec=spec) , param)
-        print("INFO:: %sGB disk added to %s" % (param['disk_size'], vm.config.name))
-        print("INFO:: VM {} add disk end.".format(param['vm_name']))
-        return 0
+        task = vm.ReconfigVM_Task(spec=spec)
+        ret = self.wait_for_task('add disk', task , param)
+        if(ret == 0):
+            print("INFO:: {} add {} GB disk success.".format(vm.config.name,param['disk_size']))
+        else :
+            print("ERROR:: {} create {} GB disk failed , reason:{} ".format(vm.config.name,param['disk_size'],task.info.error.msg))
+        return ret
 
     def add_nic(self,param):
         spec = vim.vm.ConfigSpec()
@@ -219,9 +211,7 @@ class VmManage(object):
 
         nic_spec = vim.vm.device.VirtualDeviceSpec()
         nic_spec.operation = vim.vm.device.VirtualDeviceSpec.Operation.add
-
         nic_spec.device = vim.vm.device.VirtualE1000()
-
         nic_spec.device.deviceInfo = vim.Description()
         nic_spec.device.deviceInfo.summary ='create ' + param['vm_name']
 
@@ -250,65 +240,94 @@ class VmManage(object):
 
         nic_changes.append(nic_spec)
         spec.deviceChange = nic_changes
-
-        ret = self.wait_for_task('add nic',vm.ReconfigVM_Task(spec=spec) , param)
+        task = vm.ReconfigVM_Task(spec=spec)
+        ret = self.wait_for_task('add network', task , param)
+        if(ret == 0):
+            print("INFO:: {} add network {} success.".format(vm.config.name,param['network_name']))
+        else:
+            print("ERROR:: {} add network {} failed , reason: {} ".format(vm.config.name,param['network_name'],task.info.error.msg))
+        return ret
     
     def add_cdrom(self , param):
         vm = self._get_obj([vim.VirtualMachine], param['vm_name']) 
-        controller = self.find_free_ide_controller(vm)
-        ret = 0 
-        if controller is None:
-            print('ERROR:: Failed to find a free slot on the IDE controller.')
-            ret = 1
-            return ret
+        print("INFO:: virtual machine {} Attaching iso to CD drive of ".format(param['vm_name']))
+        cdspec = None
+        for device in vm.config.hardware.device:
+            if isinstance(device, vim.vm.device.VirtualCdrom):
+                cdspec = vim.vm.device.VirtualDeviceSpec()
+                cdspec.device = device
+                cdspec.operation = vim.vm.device.VirtualDeviceSpec.Operation.edit
+                cdspec.device.backing = vim.vm.device.VirtualCdrom.IsoBackingInfo()
+                for datastore in vm.datastore:
+                    cdspec.device.backing.datastore = datastore
+                    break
+                cdspec.device.backing.fileName = param['iso_path']
+                cdspec.device.connectable = vim.vm.device.VirtualDevice.ConnectInfo()
+                cdspec.device.connectable.startConnected = True
+                cdspec.device.connectable.allowGuestControl = True
 
-        cdrom = None
-        cdrom_lun = self.get_physical_cdrom(vm.runtime.host)
-        if cdrom_lun is not None:
-            backing = vim.vm.device.VirtualCdrom.AtapiBackingInfo()
-            backing.deviceName = cdrom_lun.deviceName
-            device_spec = vim.vm.device.VirtualDeviceSpec()
-            device_spec.device = self.new_cdrom_spec(controller.key, backing)
-            device_spec.operation = vim.vm.device.VirtualDeviceSpec.Operation.add
-            config_spec = vim.vm.ConfigSpec(deviceChange=[device_spec])
-            ret = self.wait_for_task('add cdrom', vm.Reconfigure(config_spec) , param)
-
-            cdroms = self.find_device(vm, vim.vm.device.VirtualCdrom)
-            # TODO isinstance(x.backing, type(backing))
-            cdrom = next(filter(lambda x: type(x.backing) == type(backing) and
-                        x.backing.deviceName == cdrom_lun.deviceName, cdroms))
+        vmconf = vim.vm.ConfigSpec()
+        vmconf.deviceChange = [cdspec]
+        print("INFO:: virtual machine {} giving first priority for CDrom Device in boot order".format(param['vm_name']))
+        vmconf.bootOptions = vim.vm.BootOptions(bootOrder=[vim.vm.BootOptions.BootableCdromDevice()])
+        task = vm.ReconfigVM_Task(vmconf)
+        ret = self.wait_for_task('add CDrom',task ,param)
+        if(ret == 0):
+            print("INFO:: {} add CDrom success.".format(vm.config.name))
         else:
-            print('WARN:: Skipping physical CD-Rom test as no device present.')
+            print("ERROR:: {} add CDrom failed , reason: {} ".format(vm.config.name,task.info.error.msg))
+        return ret
+    
+    def add_scsi(self ,param):
+        vm = self._get_obj([vim.VirtualMachine], param['vm_name']) 
+        devices = []
+        scsi_ctr = vim.vm.device.VirtualDeviceSpec()
+        scsi_ctr.operation = vim.vm.device.VirtualDeviceSpec.Operation.add
+        scsi_ctr.device = vim.vm.device.ParaVirtualSCSIController()
+        scsi_ctr.device.deviceInfo = vim.Description()
+        scsi_ctr.device.slotInfo = vim.vm.device.VirtualDevice.PciBusSlotInfo()
+        scsi_ctr.device.slotInfo.pciSlotNumber = 16
+        scsi_ctr.device.controllerKey = 100
+        scsi_ctr.device.unitNumber = 3
+        scsi_ctr.device.busNumber = 0
+        scsi_ctr.device.hotAddRemove = True
+        scsi_ctr.device.sharedBus = 'noSharing'
+        scsi_ctr.device.scsiCtlrUnitNumber = 7
+        devices.append(scsi_ctr)
 
-        cdrom_operation = vim.vm.device.VirtualDeviceSpec.Operation
-        iso = param['iso']
-        if iso is not None:
-            device_spec = vim.vm.device.VirtualDeviceSpec()
-            if cdrom is None:  # add a cdrom
-                backing = vim.vm.device.VirtualCdrom.IsoBackingInfo(fileName=iso)
-                cdrom = self.new_cdrom_spec(controller.key, backing)
-                device_spec.operation = cdrom_operation.add
-            else:  # edit an existing cdrom
-                backing = vim.vm.device.VirtualCdrom.IsoBackingInfo(fileName=iso)
-                cdrom.backing = backing
-                device_spec.operation = cdrom_operation.edit
-            device_spec.device = cdrom
-            config_spec = vim.vm.ConfigSpec(deviceChange=[device_spec])
-            ret = self.wait_for_task('add cdrom', vm.Reconfigure(config_spec) , param)
-
-            cdroms = self.find_device(vm, vim.vm.device.VirtualCdrom)
-            # TODO isinstance(x.backing, type(backing))
-            cdrom = next(filter(lambda x: type(x.backing) == type(backing) and
-                        x.backing.fileName == iso, cdroms))
+        spec = vim.vm.ConfigSpec()
+        spec.deviceChange = devices
+        task = vm.ReconfigVM_Task(spec=spec)
+        ret = self.wait_for_task('add scsi',task ,param)
+        if(ret == 0):
+            print("INFO:: {} add scsi success.".format(vm.config.name))
         else:
-            print('WARN::Skipping ISO test as no iso provided.')
+            print("ERROR:: {} add scsi failed , reason: {} ".format(vm.config.name,task.info.error.msg))
+        return ret
 
-        #if cdrom is not None:  # Remove it
-        #    device_spec = vim.vm.device.VirtualDeviceSpec()
-        #    device_spec.device = cdrom
-        #    device_spec.operation = cdrom_operation.remove
-        #    config_spec = vim.vm.ConfigSpec(deviceChange=[device_spec])
-        #    ret = self.wait_for_task('remove cdrom', vm.Reconfigure(config_spec) , param)
+    def add_cdctr(self ,param):
+        vm = self._get_obj([vim.VirtualMachine], param['vm_name']) 
+        devices = []
+        cd_ctr = vim.vm.device.VirtualDeviceSpec()
+        cd_ctr.operation = vim.vm.device.VirtualDeviceSpec.Operation.add
+        cd_ctr.device = vim.vm.device.VirtualCdrom()
+        cd_ctr.device.deviceInfo = vim.Description()
+        cd_ctr.device.deviceInfo.label = 'CD/DVD drive'
+        cd_ctr.device.backing = vim.vm.device.VirtualCdrom.RemoteAtapiBackingInfo()
+        cd_ctr.device.connectable = vim.vm.device.VirtualDevice.ConnectInfo()
+        cd_ctr.device.connectable.startConnected = False
+        cd_ctr.device.connectable.allowGuestControl = True
+        cd_ctr.device.controllerKey = 101
+        devices.append(cd_ctr)
+        spec = vim.vm.ConfigSpec()
+        spec.deviceChange = devices
+        task = vm.ReconfigVM_Task(spec=spec)
+        ret = self.wait_for_task('add cd/dvd',task ,param)
+        if(ret == 0):
+            print("INFO:: {} add cd/dvd success.".format(vm.config.name))
+        else:
+            print("ERROR:: {} add cd/dvd failed , reason: {} ".format(vm.config.name,task.info.error.msg))
+        return ret
 
     def power_vm(self, param):
         vm = self._get_obj([vim.VirtualMachine], param['vm_name'])
@@ -351,7 +370,7 @@ class VmManage(object):
             self.wait_for_task('destroy vm', vm.Destroy_Task() , param)
         print("INFO:: {} virtual machine destroy vm.".format(param["vm_name"]))
 
-    def setCustomConfig(self , param):
+    def create_customspec(self , param):
         vm = self._get_obj([vim.VirtualMachine], param['vm_name'])
         if vm is None : 
             vm = self.content.searchIndex.FindByIp(None, param['vm_ip'], True)
@@ -360,36 +379,57 @@ class VmManage(object):
         else:
             print("INFO:: {} virtual machine powerState is: {}".format(vm.name , vm.runtime.powerState))
         
-        clonespec = vim.vm.CloneSpec()
-        # 个性化设置
-        if all([param['vm_ip'], param['netmask'], param['gateway']]):
-            clonespec.customization = self.get_customspec(param)
-        vmconf = vim.vm.ConfigSpec()
-        vmconf.cpuHotAddEnabled = True
-        vmconf.cpuHotRemoveEnabled = True
-        vmconf.memoryHotAddEnabled = True
+        adaptermap = vim.vm.customization.AdapterMapping()
+        globalip = vim.vm.customization.GlobalIPSettings()
+        adaptermap.adapter = vim.vm.customization.IPSettings()
+        adaptermap.adapter.ip = vim.vm.customization.FixedIp()
+        adaptermap.adapter.ip.ipAddress = param['vm_ip']
+        adaptermap.adapter.subnetMask = param['netmask']
+        adaptermap.adapter.gateway = param['gateway']  
+        globalip.dnsServerList = param['dns']
+        #adaptermap.adapter.dnsDomain = param['domain']
+        if param['os_type'] == 'windows':
+            prep = vim.vm.customization.Sysprep()
+            prep.guiUnattended = vim.vm.customization.GuiUnattended()
+            prep.guiUnattended.autoLogon = False
+            prep.userData = vim.vm.customization.UserData()
+            prep.userData.fullName = param['hostname']
+            prep.userData.orgName = "com"
+            prep.userData.computerName = vim.vm.customization.FixedName()
+            prep.userData.computerName.name = param['hostname']
+            prep.identification = vim.vm.customization.Identification()
+        else:
+            prep = vim.vm.customization.LinuxPrep()
+            #dent.domain = param['domain']
+            prep.hostName = vim.vm.customization.FixedName()
+            prep.hostName.name = param['vm_name']
+            prep.hwClockUTC = True
+            prep.timeZone = "Asia/Shanghai"
 
-        if param['cup_num']:
-            vmconf.numCPUs = int(param['cup_num'])
-            vmconf.numCoresPerSocket = 2
-        if param['memory']:
-            vmconf.memoryMB = int(param['memory']) * 1024
-        if vmconf is not None:
-            clonespec.config = vmconf
+        customspec = vim.vm.customization.Specification()
+        customspec.nicSettingMap = [adaptermap]
+        customspec.globalIPSettings = globalip
+        customspec.identity = prep
 
-        task = vm.ReconfigVM_Task(clonespec)
-        ret = self.wait_for_task('vm setconfig',task , param)
+        task = vm.Customize(spec=customspec)
+        ret = self.wait_for_task('vm customize',task , param)
         if ret == 0 : 
             print("INFO:: {} virtual machine set custom config success.".format(param["vm_name"]))
         else :
-            print("ERROR:: {} virtual machine set custom config success.".format(param["vm_name"]))
+            print("ERROR:: {} virtual machine set custom config failed , reason:{}.".format(param["vm_name"],task.info.error.msg))
 
+        return ret
+
+    #Todo 根据iso创建vm，没有vmtools 无法设置IP
     def create_vm(self, param):
         datacenter = self._get_obj([vim.Datacenter], param['datacenter_name'])
         if datacenter is None:
             print("ERROR:: VM datacenter {} not found .".format(param['datacenter_name']))
             exit(1)
         vmfolder = datacenter.vmFolder
+        folder  = self._get_folder(param['vm_folder'])
+        if folder is not None :
+            vmfolder = folder
 
         cluster = self.get_cluster_by_name(param['cluster_name'], datacenter)
         if cluster is None:
@@ -430,21 +470,11 @@ class VmManage(object):
 
         config = vim.vm.ConfigSpec()
         config.name = param['vm_name']
-        #config.powerOn = True
         config.cpuHotAddEnabled = True
         config.cpuHotRemoveEnabled = True
         config.memoryHotAddEnabled = True
         #操作系统版本
         config.guestId = param['guest']
-
-        tools = vim.vm.ToolsConfigInfo()
-        tools.afterPowerOn = True
-        #新建虚拟机个性化设置
-        #if all([param['vm_ip'], param['netmask'], param['gateway']]):
-        #    customization = self.get_customspec(param)
-        #    tools.pendingCustomization = customization
-        config.tools = tools
-
         if param['cup_num']:
             config.numCPUs = int(param['cup_num'])
             config.numCoresPerSocket = 2
@@ -460,23 +490,30 @@ class VmManage(object):
             ret = self.wait_for_task('create vm',task , param)
             if (ret == 0):
                 print("INFO:: VM {} created success .".format(param['vm_name']))
+                #网络
                 if param['network_name'] is not None :
-                    self.add_nic(param)
-
-                if param['disk_size'] is not None :
-                    ret = self.add_disk(param)
-                    if ret == 0 :
-                        print("INFO:: {} add {} {} disk success.".format(param['vm_name'] , param['disk_type'] , param['disk_size']))
-                    else :
-                        print("ERROR:: {} add {} type {} disk failed.".format(param['vm_name'] , param['disk_type'] , param['disk_size']))
-
-                if param['iso'] is not None : 
-                    self.add_cdrom(param)
+                    ret = self.add_nic(param)
                 
+                #磁盘
+                if param['disk_size'] is not None :
+                    ret = self.add_scsi(param)
+                    if ret == 0 :
+                        self.add_disk(param)
+                
+                #设置bootloader
+                if param['iso_path'] is not None : 
+                    ret = self.add_cdctr(param)
+                    if ret == 0 :
+                        ret = self.add_cdrom(param)
+                        if ret == 0 :
+                            param['power_type'] == 'powerOn'
+                            self.power_vm(param)
+                
+                #个性化设置
                 if all([param['vm_ip'], param['netmask'], param['gateway']]):
-                    self.setCustomConfig(param)
+                    ret = self.create_customspec(param)
             else:
-                print("ERROR:: VM {} created failed ,reason:{} .".format(param['vm_name']),task.info.error.msg)
+                print("ERROR:: VM {} created failed ,reason:{} .".format(param['vm_name'] ,task.info.error.msg))
 
         except vim.fault.DuplicateName:
             print("ERROR:: VM duplicate name: %s" % param['vm_name'], file=sys.stderr)
@@ -494,7 +531,13 @@ class VmManage(object):
         if datacenter is None:
             print("ERROR:: VM datacenter {} not found .".format(param['datacenter_name']))
             exit(1)
+
         vmfolder = datacenter.vmFolder
+        if param['vm_folder']:
+            self.create_folder(vmfolder,param['vm_folder'])
+            folder  = self._get_folder(param['vm_folder'])
+            if folder is not None :
+                vmfolder = folder
 
         if param['datastore_name']:
             datastore = self.get_datastore_by_name(param['datastore_name'], datacenter)
@@ -537,7 +580,7 @@ class VmManage(object):
 
         # 克隆个性化设置
         if all([param['vm_ip'], param['netmask'], param['gateway']]):
-            clonespec.customization = self.get_customspec(param)
+            clonespec.customization = self.clone_customspec(param)
         vmconf = vim.vm.ConfigSpec()
         vmconf.cpuHotAddEnabled = True
         vmconf.cpuHotRemoveEnabled = True
