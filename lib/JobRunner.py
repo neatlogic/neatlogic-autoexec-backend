86b1a4fc5ea0c42059e5844f7a496ad0d495fdd2
温海波 <wenhb@techsure.com.cn>
2025-06-09 17:34:50 +0800

    clear event log

--- lib/JobRunner.py
+++ lib/JobRunner.py
@@ -377,6 +377,7 @@ class JobRunner:
             phaseStatus = self.context.phases[phaseName]
             phaseStatus.clearRoundFinEvent()
             phaseStatus.clearGlobalRoundFinEvent()
+            print("INFO: Clear phase:{} round event.\n".format(phaseName))
 
     def _deducePhaseStatus(self, phaseStatus):
         endStatus = NodeStatus.aborted
